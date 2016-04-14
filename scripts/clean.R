be66882a60ca260fca181954bd3ba837a554a46c
Kala Sundararajan <ksundara@ads.uhnresearch.ca>
2016-04-14 16:08:21 -0400

    initial commit

---
+++ scripts/clean.R
@@ -0,0 +1,9 @@
+################################### Project: add survey data to biomarker validation data #####################################
+# date: 2016-04-14
+# written by: Kala Sundararajan
+# biobank sample data from Rajiv Gandhi
+# biobank survey data from Daniel Antflek
+# LEAP BL and 3m survey data from DADOS
+
+
+########################### clean.R: clean/merge data ###############################
---
+++ scripts/load.R
@@ -0,0 +1,8 @@
+################################### Project: add survey data to biomarker validation data #####################################
+# date: 2016-04-14
+# written by: Kala Sundararajan
+# biobank sample data from Rajiv Gandhi
+# biobank survey data from Daniel Antflek
+# LEAP BL and 3m survey data from DADOS
+
+########################### load.R: load raw data ###############################
