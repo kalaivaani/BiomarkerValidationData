2e40a7feb3147e4a5f4699af70377b5e996c68ef
Kala Sundararajan <ksundara@ads.uhnresearch.ca>
2016-04-15 09:09:07 -0400

    - merge together serum and SF data, add LEAP IDs

--- scripts/clean.R
+++ scripts/clean.R
@@ -7,3 +7,28 @@
 
 
 ########################### clean.R: clean/merge data ###############################
+
+# create clean IDs for biobank datasets
+BioMaster$Study.ID <- clean_bioID(BioMaster$Study.ID)
+Serum$ID <- clean_bioID(Serum$ID)
+SF$ID <- clean_bioID(SF$ID)
+Bio_BL$Study.ID <- clean_bioID(Bio_BL$Study.ID)
+Bio_3m$Study.ID <- clean_bioID(Bio_3m$Study.ID)
+
+# prefix serum and SF variables
+names(Serum)[-1] <- paste0("Serum_",names(Serum)[-1])
+names(SF)[-1] <- paste0("SF_",names(SF)[-1])
+
+# merge Serum and SF data
+biomarkers <- merge(Serum, SF, by="ID", all=T)
+
+# subset Knee data
+biomarkers <- subset(biomarkers, biomarkers$ID %in% BioMaster$Study.ID)
+
+# merge LEAP IDs from biobank master list
+BioMaster[BioMaster == "" | BioMaster=="NA" | BioMaster=="?"| BioMaster=="Pending"] <- NA
+biomarkers <- merge(biomarkers, BioMaster[,c("Study.ID", "LEAP.ID")], by.x="ID", by.y="Study.ID", all.x=TRUE, all.y=FALSE)
+
+# select BL and 3m data from LEAP
+LEAP_BL <- LEAPKnee[,c(grep("SUBJECT_ID", names(LEAPKnee)),grep("BASELINE_", names(LEAPKnee)))]
+LEAP_3m <- LEAPKnee[,c(grep("SUBJECT_ID", names(LEAPKnee)),grep("X3MONTH_", names(LEAPKnee)))]
